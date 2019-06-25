5113e26c4b6d1044a43d148abad194a171dfaae7
raymondhechen <rc284@duke.edu>
2019-06-24 20:03:07 -0400

    Completed modals

---
+++ scripts/rmodal.js
@@ -0,0 +1,26 @@
+// When the user clicks on the button, open the modal 
+function displayModal(i) {
+    document.getElementById(i).style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal(i) {
+    document.getElementById(i).style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+document.onclick = function (event) {
+    var cards = ["rmodal1","rmodal2","rmodal3","rmodal4","rmodal5","rmodal6","pmodal1"];
+    var curr;
+
+    for (var i=0; i < cards.length; i++) {
+        var div = document.getElementById(cards[i]);
+        if (div.style.display == "block") {
+            curr = div;
+        }
+    }
+
+    if (event.target == curr) {
+        curr.style.display = "none";
+    }
+}
