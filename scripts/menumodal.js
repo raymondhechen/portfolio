1d29150eb4634728616b2f31805f6285b46445e6
raymondhechen <rc284@duke.edu>
2019-06-27 21:42:21 -0400

    Completed menu modal

--- scripts/menumodal.js
+++ scripts/menumodal.js
@@ -7,3 +7,34 @@ function loadMenuModal() {
 function closeMenuModal() {
     document.getElementById("mmodal").style.display = "none";
 }
+
+function changePageMobile(nextID) {
+    // vars
+    var bodyIDs = ["main", "resume", "research", "projects"];
+    var curr = document.getElementById("main");
+
+    // Find current element
+    for (var i = 0; i < bodyIDs.length; i++) {
+        body = document.getElementById(bodyIDs[i]);
+        if (body.style.display == "flex" || body.style.display == "block") {
+            curr = body;
+            break;
+        }
+    }
+
+    // Get next element
+    var next = document.getElementById(nextID);
+    curr.style.display = "none"; // Remove curr display
+
+    // Turn on next display
+    if (next == document.getElementById("resume")) {
+        next.style.display = "block";
+    }
+    else {
+        next.style.display = "flex";
+    }
+
+    curr.style.opacity = 0.1; // Set curr opacity to original
+    document.getElementById("mmodal").style.display = "none";   
+    fadeIn(next); // Fade in next
+}
