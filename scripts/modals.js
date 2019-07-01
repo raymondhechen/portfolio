6a1e5135fea6b8e4eb77cf8427f7790e84267e63
raymondhechen <rc284@duke.edu>
2019-07-01 18:48:40 -0400

    Added modal transitions

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,11 +1,13 @@
 // When the user clicks on the button, open the modal 
 function displayModal(i) {
     document.getElementById(i).style.display = "block";
+    modalFadeIn(document.getElementById(i));
 }
 
 // When the user clicks on <span> (x), close the modal
 function closeModal(i) {
     document.getElementById(i).style.display = "none";
+    modalFadeOut(document.getElementById(i));
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -25,9 +27,34 @@ document.onclick = function (event) {
         }
 
         if (event.target == curr) {
-            curr.style.display = "none";
+            modalFadeOut(curr);
+            //curr.style.display = "none";
         }
     }
+}
+
+// Fade in Function
+function modalFadeIn(element) {
+    var op = 0.0;  // initial opacity
+    var timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        op += 0.1;
+    }, 10);
+}
+
+function modalFadeOut(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0) {
+            clearInterval(timer);
+            element.style.display = "none";
+        }
+        element.style.opacity = op;
+        op -= 0.1;
+    }, 10);
 }
 
 function menufadeOut(element) {
