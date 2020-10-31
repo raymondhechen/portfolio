3872f1c2d687bb11efc011d2684885b6b28c4eb6
Raymond Chen <rc284@duke.edu>
2020-10-31 03:42:39 -0400

    v1 upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Home from './components/Home'
+import About from './components/About'
+import Resume from './components/Resume'
+import Projects from './components/Projects'
+import Research from './components/Research'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+          
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return(
+            <div class="topnav">
+                <a class="Home" href="#home">Home</a>
+                <a href="Projects">News</a>
+                <a href="Research">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        )
+    }
+}
