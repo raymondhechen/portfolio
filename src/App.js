a5f0f0ba5527572995e204c1210d6e6331487402
raymondhechen <rc284@duke.edu>
2021-10-12 15:38:35 -0400

    added resume

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ZoomPage from './pages/Zoom';
 import CalendlyPage from './pages/Calendly';
+import ResumePage from './pages/Resume';
 
 function App() {
     return (
         <Switch>
-            <Route exact path="/" component={HomePage} />
             <Route exact path="/zoom" component={ZoomPage} />
             <Route exact path="/calendly" component={CalendlyPage} />
+            <Route exact path="/resume" component={ResumePage} />
+            <Route path="/" component={HomePage} />
         </Switch>
     );
 }
