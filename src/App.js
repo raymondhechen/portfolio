c1612ed1301cabcd75c29675b61e550f2fe21424
raymondhechen <rc284@duke.edu>
2021-10-13 00:55:45 -0400

    intro link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/Home';
 import ZoomPage from './pages/Zoom';
 import CalendlyPage from './pages/Calendly';
 import ResumePage from './pages/Resume';
+import IntroPage from './pages/Intro';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Route exact path="/zoom" component={ZoomPage} />
             <Route exact path="/calendly" component={CalendlyPage} />
             <Route exact path="/resume" component={ResumePage} />
+            <Route exact path="/intro" component={IntroPage} />
             <Route path="/" component={HomePage} />
         </Switch>
     );
