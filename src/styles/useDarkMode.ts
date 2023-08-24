56339604541e0dbb1c45b699d2d1c9235653f4c0
Raymond Chen <raymondhechen@gmail.com>
2023-08-23 18:26:41 -0700

    try without localstorage

--- src/styles/useDarkMode.ts
+++ src/styles/useDarkMode.ts
@@ -2,30 +2,18 @@
 
 import { useEffect, useState } from 'react'
 
-const parseBoolean = (boolString: string) => {
-  return boolString === 'true'
-}
-
 const useDarkMode = () => {
   const [isDarkTheme, setIsDarkTheme] = useState<boolean>(true)
 
   useEffect(() => {
-    const storedDarkMode = localStorage.getItem('dark')
-    if (storedDarkMode !== null) {
-      setIsDarkTheme(parseBoolean(storedDarkMode))
-      return
-    }
-
     const prefersLightTheme = window.matchMedia('(prefers-color-scheme: light)')
     if (prefersLightTheme.matches) {
       setIsDarkTheme(false)
-      localStorage.setItem('dark', 'false')
     }
   }, [])
 
   const toggleTheme = () => {
     setIsDarkTheme(!isDarkTheme)
-    localStorage.setItem('dark', `${!isDarkTheme}`)
   }
 
   return { isDarkTheme, toggleTheme }
