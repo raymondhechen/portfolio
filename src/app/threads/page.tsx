0db29c6b91162d8fc9abc83b3fd26c6dbdcc2bd8
Raymond Chen <raymondhechen@gmail.com>
2023-08-26 11:53:10 -0700

    [feat] threads repository

--- src/app/threads/page.tsx
+++ src/app/threads/page.tsx
@@ -7,30 +7,30 @@ import { useContext } from 'react'
 import { Tweet } from 'react-tweet'
 import { styled } from 'styled-components'
 import '@/tweets/Tweet.css'
+import { threads } from './repository'
 
 const Content = styled.div`
   display: flex;
   flex-wrap: wrap;
+  gap: 25px;
   padding: 30px;
   width: 100%;
-  height: 50%;
+  height: 100%;
 `
 
-const TweetContainer = styled.div`
-  width: 100%;
-  height: 50%;
-`
+const TweetContainer = styled.div``
 
 const ThreadsPage = () => {
   const isDarkTheme = useContext(DarkModeContext)
-  console.log({ isDarkTheme })
 
   return (
     <Page>
       <Content>
-        <TweetContainer className={isDarkTheme ? 'dark' : 'light'}>
-          <Tweet id="1694424219902443583" components={tweetComponents} />
-        </TweetContainer>
+        {threads.map((thread) => (
+          <TweetContainer className={isDarkTheme ? 'dark' : 'light'}>
+            <Tweet id={thread.src} components={tweetComponents} />
+          </TweetContainer>
+        ))}
       </Content>
     </Page>
   )
---
+++ src/app/threads/repository.ts
@@ -0,0 +1,15 @@
+type Thread = {
+  src: string
+  title: string
+}
+
+export const threads = [
+  {
+    src: '1695508662272585769',
+    title: 'Tuned Mass Dampers',
+  },
+  {
+    src: '1694424219902443583',
+    title: 'Sequoia National Park',
+  },
+]
