fe8453f71c51bd4eacc21686ba93f4fd23b243e7
Raymond Chen <raymondhechen@gmail.com>
2024-05-28 12:09:12 -0700

    add meeting link

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/meeting',
+        destination: 'https://calendar.app.google/VmEvDdygEC2HmGwo8',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'pbs.twimg.com' },
